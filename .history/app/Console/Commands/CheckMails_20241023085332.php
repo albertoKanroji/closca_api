c899773e0868f7269c66da89594f9f9f33eec6c7
Alberto de Jesús Hernández <luismiq123456789@gmail.com>
2024-10-31 09:52:34 -0600

    x

--- .history/app/Console/Commands/CheckMails_20241023085332.php
+++ .history/app/Console/Commands/CheckMails_20241023085332.php
@@ -11,7 +11,6 @@ use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Storage;
 use App\Models\Auto;
 use App\Models\Imagen;
-
 class CheckMails extends Command
 {
     protected $signature = 'mails:check';
@@ -92,7 +91,7 @@ class CheckMails extends Command
                                 // Guardar en la base de datos
                                 $this->info('Archivo JSON guardado en: ' . $filePath);
                                 Log::info('Archivo JSON guardado en: ' . $filePath);
-                                // *** PROCESAR EL CONTENIDO DEL JSON GUARDADO ***
+                                 // *** PROCESAR EL CONTENIDO DEL JSON GUARDADO ***
 
                                 // Leer el contenido del archivo JSON
                                 $jsonContent = Storage::get("$folderName/{$attachment->name}");
@@ -102,87 +101,85 @@ class CheckMails extends Command
                                 foreach ($data['inspecciones'] as $inspeccion) {
                                     $autoData = $inspeccion['auto'];
 
-                                    // Usar updateOrInsert para evitar duplicados
-                                    Auto::updateOrInsert(
-                                        ['id_auto' => $autoData['Unidad']['id_auto']], // Condición para actualizar o insertar
-                                        [
-                                            'vin' => $autoData['Unidad']['vin'],
-                                            'f_ingreso' => $autoData['Unidad']['f_ingreso'],
-                                            'f_salida' => $autoData['Unidad']['f_salida'],
-                                            'id_cliente' => $autoData['Unidad']['id_cliente'],
-                                            'id_contacto' => $autoData['Unidad']['id_contacto'],
-                                            'id_marca' => $autoData['Unidad']['id_marca'],
-                                            'modelo' => $autoData['Unidad']['modelo'],
-                                            'color' => $autoData['Unidad']['color'],
-                                            'color_ext' => $autoData['Unidad']['color_ext'],
-                                            'lavado_presion' => $autoData['Unidad']['lavado_presion'],
-                                            'voltajeBateria' => $autoData['Unidad']['voltajeBateria'],
-                                            'barco' => $autoData['Unidad']['barco'],
-                                            'modelo_ext' => $autoData['Unidad']['modelo_ext'],
-                                            'dealer' => $autoData['Unidad']['dealer'],
-                                            'firma' => $autoData['Unidad']['firma'],
-                                            // UBICACION
-                                            'pais_destino' => $autoData['UBICACION']['pais_destino'],
-                                            'id_patio' => $autoData['UBICACION']['id_patio'],
-                                            'fila' => $autoData['UBICACION']['fila'],
-                                            'viaje' => $autoData['UBICACION']['viaje'],
-                                            'posicion' => $autoData['UBICACION']['posicion'],
-                                            '_localizacion' => $autoData['UBICACION']['_localizacion'],
-                                            'referencia' => $autoData['UBICACION']['referencia'],
-                                            // DMG
-                                            'dmg_codigo' => $autoData['DMG']['dmg_codigo'],
-                                            'dmg_clasificacion' => $autoData['DMG']['dmg_clasificacion'],
-                                            'dmg_descripcion' => $autoData['DMG']['dmg_descripcion'],
-                                            'dmg_modo' => $autoData['DMG']['dmg_modo'],
-                                            'dmg_maniobra' => $autoData['DMG']['dmg_maniobra'],
-                                            'dmg_transporte' => $autoData['DMG']['dmg_transporte'],
-                                            'dmg_responsable' => $autoData['DMG']['dmg_responsable'],
-                                            // REPUVE
-                                            'rep_reparable' => $autoData['REPUVE']['rep_reparable'],
-                                            'rep_responsable' => $autoData['REPUVE']['rep_responsable'],
-                                            'rep_fecha_autorizacion' => $autoData['REPUVE']['rep_fecha_autorizacion'],
-                                            'rep_fecha_liberacion' => $autoData['REPUVE']['rep_fecha_liberacion'],
-                                            'rep_dias' => $autoData['REPUVE']['rep_dias'],
-                                            'rep_orden_servicio' => $autoData['REPUVE']['rep_orden_servicio'],
-                                            'rep_requiere_partes' => $autoData['REPUVE']['rep_requiere_partes'],
-                                            'rep_fecha_orden_partes' => $autoData['REPUVE']['rep_fecha_orden_partes'],
-                                            'rep_control_pedido' => $autoData['REPUVE']['rep_control_pedido'],
-                                            'rep_fecha_entrega_partes' => $autoData['REPUVE']['rep_fecha_entrega_partes'],
-                                            'rep_fecha_termino' => $autoData['REPUVE']['rep_fecha_termino'],
-                                            'rep_estado' => $autoData['REPUVE']['rep_estado'],
-                                            'rep_comentarios' => $autoData['REPUVE']['rep_comentarios'],
-                                            'rep_sitio' => $autoData['REPUVE']['rep_sitio'],
-                                            // ACCESORIOS
-                                            'ac_llave_tarjeta' => $autoData['ACCESORIOS']['ac_llave_tarjeta'],
-                                            'ac_tarjeta_memoria' => $autoData['ACCESORIOS']['ac_tarjeta_memoria'],
-                                            'ac_7KW_charger' => $autoData['ACCESORIOS']['ac_7KW_charger'],
-                                            'ac_chaleco_reflectante' => $autoData['ACCESORIOS']['ac_chaleco_reflectante'],
-                                            'ac_triangulo_adv' => $autoData['ACCESORIOS']['ac_triangulo_adv'],
-                                            'ac_montaje_gato' => $autoData['ACCESORIOS']['ac_montaje_gato'],
-                                            'ac_gancho_remolque' => $autoData['ACCESORIOS']['ac_gancho_remolque'],
-                                            'ac_gancho_traccion' => $autoData['ACCESORIOS']['ac_gancho_traccion'],
-                                            'ac_llave_inteligente' => $autoData['ACCESORIOS']['ac_llave_inteligente'],
-                                            'ac_pinza_desmontaje_dec' => $autoData['ACCESORIOS']['ac_pinza_desmontaje_dec'],
-                                            'ac_red_neumatico' => $autoData['ACCESORIOS']['ac_red_neumatico'],
-                                            'ac_limpiaparabrisas_izq' => $autoData['ACCESORIOS']['ac_limpiaparabrisas_izq'],
-                                            'ac_llave_inglesa' => $autoData['ACCESORIOS']['ac_llave_inglesa'],
-                                            'ac_cierre' => $autoData['ACCESORIOS']['ac_cierre'],
-                                            'ac_limpiaparabrisas_der' => $autoData['ACCESORIOS']['ac_limpiaparabrisas_der'],
-                                            'ac_clips_decorativos' => $autoData['ACCESORIOS']['ac_clips_decorativos'],
-                                            'ac_manual_usuario' => $autoData['ACCESORIOS']['ac_manual_usuario'],
-                                            'ac_barra_remota_gato' => $autoData['ACCESORIOS']['ac_barra_remota_gato'],
-                                            // COMENTARIOS
-                                            'comentario_1' => $autoData['COMENTARIOS']['comentario_1'],
-                                            'comentario_2' => $autoData['COMENTARIOS']['comentario_2'],
-                                            'comentario_3' => $autoData['COMENTARIOS']['comentario_3'],
-                                        ]
-                                    );
+                                    // Guardar los datos del vehículo en el modelo Auto
+                                    $auto = Auto::create([
+                                        'id_auto' => $autoData['Unidad']['id_auto'],
+                                        'vin' => $autoData['Unidad']['vin'],
+                                        'f_ingreso' => $autoData['Unidad']['f_ingreso'],
+                                        'f_salida' => $autoData['Unidad']['f_salida'],
+                                        'id_cliente' => $autoData['Unidad']['id_cliente'],
+                                        'id_contacto' => $autoData['Unidad']['id_contacto'],
+                                        'id_marca' => $autoData['Unidad']['id_marca'],
+                                        'modelo' => $autoData['Unidad']['modelo'],
+                                        'color' => $autoData['Unidad']['color'],
+                                        'color_ext' => $autoData['Unidad']['color_ext'],
+                                        'lavado_presion' => $autoData['Unidad']['lavado_presion'],
+                                        'voltajeBateria' => $autoData['Unidad']['voltajeBateria'],
+                                        'barco' => $autoData['Unidad']['barco'],
+                                        'modelo_ext' => $autoData['Unidad']['modelo_ext'],
+                                        'dealer' => $autoData['Unidad']['dealer'],
+                                        'firma' => $autoData['Unidad']['firma'],
+                                        // UBICACION
+                                        'pais_destino' => $autoData['UBICACION']['pais_destino'],
+                                        'id_patio' => $autoData['UBICACION']['id_patio'],
+                                        'fila' => $autoData['UBICACION']['fila'],
+                                        'viaje' => $autoData['UBICACION']['viaje'],
+                                        'posicion' => $autoData['UBICACION']['posicion'],
+                                        '_localizacion' => $autoData['UBICACION']['_localizacion'],
+                                        'referencia' => $autoData['UBICACION']['referencia'],
+                                        // DMG
+                                        'dmg_codigo' => $autoData['DMG']['dmg_codigo'],
+                                        'dmg_clasificacion' => $autoData['DMG']['dmg_clasificacion'],
+                                        'dmg_descripcion' => $autoData['DMG']['dmg_descripcion'],
+                                        'dmg_modo' => $autoData['DMG']['dmg_modo'],
+                                        'dmg_maniobra' => $autoData['DMG']['dmg_maniobra'],
+                                        'dmg_transporte' => $autoData['DMG']['dmg_transporte'],
+                                        'dmg_responsable' => $autoData['DMG']['dmg_responsable'],
+                                        // REPUVE
+                                        'rep_reparable' => $autoData['REPUVE']['rep_reparable'],
+                                        'rep_responsable' => $autoData['REPUVE']['rep_responsable'],
+                                        'rep_fecha_autorizacion' => $autoData['REPUVE']['rep_fecha_autorizacion'],
+                                        'rep_fecha_liberacion' => $autoData['REPUVE']['rep_fecha_liberacion'],
+                                        'rep_dias' => $autoData['REPUVE']['rep_dias'],
+                                        'rep_orden_servicio' => $autoData['REPUVE']['rep_orden_servicio'],
+                                        'rep_requiere_partes' => $autoData['REPUVE']['rep_requiere_partes'],
+                                        'rep_fecha_orden_partes' => $autoData['REPUVE']['rep_fecha_orden_partes'],
+                                        'rep_control_pedido' => $autoData['REPUVE']['rep_control_pedido'],
+                                        'rep_fecha_entrega_partes' => $autoData['REPUVE']['rep_fecha_entrega_partes'],
+                                        'rep_fecha_termino' => $autoData['REPUVE']['rep_fecha_termino'],
+                                        'rep_estado' => $autoData['REPUVE']['rep_estado'],
+                                        'rep_comentarios' => $autoData['REPUVE']['rep_comentarios'],
+                                        'rep_sitio' => $autoData['REPUVE']['rep_sitio'],
+                                        // ACCESORIOS
+                                        'ac_llave_tarjeta' => $autoData['ACCESORIOS']['ac_llave_tarjeta'],
+                                        'ac_tarjeta_memoria' => $autoData['ACCESORIOS']['ac_tarjeta_memoria'],
+                                        'ac_7KW_charger' => $autoData['ACCESORIOS']['ac_7KW_charger'],
+                                        'ac_chaleco_reflectante' => $autoData['ACCESORIOS']['ac_chaleco_reflectante'],
+                                        'ac_triangulo_adv' => $autoData['ACCESORIOS']['ac_triangulo_adv'],
+                                        'ac_montaje_gato' => $autoData['ACCESORIOS']['ac_montaje_gato'],
+                                        'ac_gancho_remolque' => $autoData['ACCESORIOS']['ac_gancho_remolque'],
+                                        'ac_gancho_traccion' => $autoData['ACCESORIOS']['ac_gancho_traccion'],
+                                        'ac_llave_inteligente' => $autoData['ACCESORIOS']['ac_llave_inteligente'],
+                                        'ac_pinza_desmontaje_dec' => $autoData['ACCESORIOS']['ac_pinza_desmontaje_dec'],
+                                        'ac_red_neumatico' => $autoData['ACCESORIOS']['ac_red_neumatico'],
+                                        'ac_limpiaparabrisas_izq' => $autoData['ACCESORIOS']['ac_limpiaparabrisas_izq'],
+                                        'ac_llave_inglesa' => $autoData['ACCESORIOS']['ac_llave_inglesa'],
+                                        'ac_cierre' => $autoData['ACCESORIOS']['ac_cierre'],
+                                        'ac_limpiaparabrisas_der' => $autoData['ACCESORIOS']['ac_limpiaparabrisas_der'],
+                                        'ac_clips_decorativos' => $autoData['ACCESORIOS']['ac_clips_decorativos'],
+                                        'ac_manual_usuario' => $autoData['ACCESORIOS']['ac_manual_usuario'],
+                                        'ac_barra_remota_gato' => $autoData['ACCESORIOS']['ac_barra_remota_gato'],
+                                        // COMENTARIOS
+                                        'comentario_1' => $autoData['COMENTARIOS']['comentario_1'],
+                                        'comentario_2' => $autoData['COMENTARIOS']['comentario_2'],
+                                        'comentario_3' => $autoData['COMENTARIOS']['comentario_3'],
+                                    ]);
 
                                     // Guardar las imágenes si existen
                                     if (!empty($inspeccion['imagenes'])) {
                                         foreach ($inspeccion['imagenes'] as $imagen) {
                                             Imagen::create([
-                                                'folio' => $autoData['Unidad']['id_auto'],
+                                                'folio' => $auto->id_auto,
                                                 'id_imagen' => $imagen['id_imagen'],
                                                 '_key' => $imagen['_key'],
                                                 'adjunto' => $imagen['adjunto'],
@@ -213,9 +210,9 @@ class CheckMails extends Command
                                     Log::info('Correo con adjunto .json guardado en la base de datos con clave de seguimiento.');
 
                                     // Enviar la respuesta automática
-                                    Mail::raw("SE HA GUARDADO CORRECTAMENTE EL JSON EN NUESTRA BASE DE DATOS.\n\nCLAVE DE SEGUIMIENTO: $trackingHash", function ($message) use ($fromEmail) {
+                                    Mail::raw("SE HA GUARDADO CORRECTAMENTE EL JSON EN NUESTRA BASE DE DATOS.\n\nCLAVE DE SEGUIMIENTO: $trackingHash", function($message) use ($fromEmail) {
                                         $message->to($fromEmail)
-                                            ->subject('Confirmación de recepción de JSON');
+                                                ->subject('Confirmación de recepción de JSON');
                                     });
 
                                     $this->info('Respuesta enviada a: ' . $fromEmail);
